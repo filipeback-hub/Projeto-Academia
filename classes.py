3565b40312895155ecea71e540a899fbea140594
Filipe Back <filipedelimaback@gmail.com>
2026-09-14 14:27:32 -0300

    Menu e cadastro de alunos e professores
    Criei um menu interativo e fiz as opcoes de cadastrar alunos e de cadastrar professores

--- classes.py
+++ classes.py
@@ -14,3 +14,19 @@ class Aluno(Pessoa):
 class Professor(Pessoa):
     def __init__(self, nome, idade):
         super().__init__(nome, idade)
+
+
+
+class Academia:
+    def __init__(self):
+        self.alunos = []
+        self.professores = []
+
+    def cadastrar_aluno(self,aluno):
+        self.alunos.append(aluno)
+        print(f'Aluno {aluno.nome}({aluno.idade}) cadastrado com sucesso')
+
+
+    def cadastrar_prof(self,prof):
+        self.professores.append(prof)
+        print(f'Professor {prof.nome}({prof.idade}) cadastrado com sucesso')
--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 from classes import *
 
-def main():
-    pass
+def menu():
+    print('''==== ACADEMIA BACKEND ====
+    1- Cadastrar aluno
+    2- Cadastrar professor
+    3- Listar alunos
+    4- Listar Professores
+    5- Sair''')
 
-if __name__ == '__main--':
+    opcao = input('Escolha: ')
+    return opcao
+
+def cadastro_aluno():
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    aluno = Aluno(nome,idade)
+    gym = Academia()
+    gym.cadastrar_aluno(aluno)
+
+def cadastro_professor():
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    prof = Professor(nome,idade)
+    gym = Academia()
+    gym.cadastrar_prof(prof)
+
+
+def main():
+    while True:
+        opc = menu()
+        print(f'Opção escolhida: {opc}')
+
+        if opc == '1':
+            cadastro_aluno()
+
+        if opc == '2':
+            cadastro_professor()
+
+
+        if opc == '5':
+            break
+
+
+if __name__ == '__main__':
     main()
